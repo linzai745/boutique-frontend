3084989b2e162503e62e372a767009293b7f9f0d
linzai745 <alin_hub@yeah.net>
2021-01-27 23:39:32 +0800

    login compoent

--- projects/boutique-frontend/src/app/components/components.module.ts
+++ projects/boutique-frontend/src/app/components/components.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavcartComponent } from './navcart/navcart.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, NavcartComponent, FooterComponent],
+  declarations: [NavbarComponent, NavcartComponent, FooterComponent, LoginComponent],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ]
 })
 export class ComponentsModule { }
