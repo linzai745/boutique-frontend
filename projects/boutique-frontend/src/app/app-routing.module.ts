7547d062eda1c85e08317d081efe9fd70418d44f
linzai745 <alin_hub@yeah.net>
2021-03-02 12:20:46 +0800

    boutique-front

--- projects/boutique-frontend/src/app/app-routing.module.ts
+++ projects/boutique-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component'
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '', component: LayoutComponent, children: [{path: '', loadChildren: ()=>import('./layout/layout.module').then(m => m.LayoutModule)}]},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: RegistrationComponent}
   // {path: '**', redirectTo: '/home'}
 ];
 
